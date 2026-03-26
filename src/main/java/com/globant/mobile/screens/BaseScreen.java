dd4c00998406359f05c1868c53218fb280069447
Juan Andrés Porras Vélez <j.porras@globant.com>
2026-03-26 15:17:07 -0500

    Added basic connection test for appium - All good!

--- src/main/java/com/globant/mobile/screens/BaseScreen.java
+++ src/main/java/com/globant/mobile/screens/BaseScreen.java
@@ -1,4 +1,18 @@
 package com.globant.mobile.screens;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
 public class BaseScreen {
+    protected AndroidDriver driver;
+    protected WebDriverWait wait;
+
+    public BaseScreen(AndroidDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
 }
--- src/test/java/com/globant/mobile/tests/BaseTest.java
+++ src/test/java/com/globant/mobile/tests/BaseTest.java
@@ -1,4 +1,33 @@
 package com.globant.mobile.tests;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
 public class BaseTest {
+    protected AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setDeviceName("emulator-5554"); // Cambia por tu ID de 'adb devices'
+        options.setAutomationName("UiAutomator2");
+        options.setApp("C:\\Users\\j.porras\\Documents\\Final_Project\\Mobile_Testing_Final\\src\\test\\resources\\apps\\android.wdio.native.app.v2.2.0.apk");
+        options.setAppWaitActivity("com.wdiodemoapp.MainActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
