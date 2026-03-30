a409273e529cba61031dd47d5f3f40c99e3dc6af
Juan Andrés Porras Vélez <j.porras@globant.com>
2026-03-29 23:21:10 -0500

    Added full signup test and updated login screen for login and signup management

--- src/main/java/com/globant/mobile/screens/LoginScreen.java
+++ src/main/java/com/globant/mobile/screens/LoginScreen.java
@@ -3,6 +3,7 @@ package com.globant.mobile.screens;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginScreen extends BaseScreen {
     @AndroidFindBy(accessibility = "Login-screen")
@@ -11,5 +12,54 @@ public class LoginScreen extends BaseScreen {
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Login / Sign up Form\")")
     public WebElement loginTitle;
 
-    public LoginScreen(AndroidDriver driver) { super(driver); }
+    // Selectores para el Switch superior
+    @AndroidFindBy(accessibility = "button-login-container")
+    public WebElement loginTab;
+
+    @AndroidFindBy(accessibility = "button-sign-up-container")
+    public WebElement signUpTab;
+
+    // Campos del Formulario
+    @AndroidFindBy(accessibility = "input-email")
+    public WebElement emailField;
+
+    @AndroidFindBy(accessibility = "input-password")
+    public WebElement passwordField;
+
+    @AndroidFindBy(accessibility = "input-repeat-password")
+    public WebElement repeatPasswordField;
+
+    // Botones de acción
+    @AndroidFindBy(accessibility = "button-LOGIN")
+    public WebElement loginButton;
+
+    @AndroidFindBy(accessibility = "button-SIGN UP")
+    public WebElement signUpButton;
+
+    // Alertas/Mensajes de éxito
+    @AndroidFindBy(id = "android:id/message")
+    public WebElement successMessage;
+
+    @AndroidFindBy(id = "android:id/button1")
+    public WebElement okButton;
+
+    public LoginScreen(AndroidDriver driver) {
+        super(driver);
+    }
+
+    public void clickSignUpTab() { signUpTab.click(); }
+    public void clickLoginTab() { loginTab.click(); }
+    public void clickSignUpButton() { signUpButton.click(); }
+    public void clickLoginButton() { loginButton.click(); }
+    public void clickOkButton() { okButton.click(); }
+
+    public void fillSignUpForm(String email, String password) {
+        sendKeysSafely(emailField, email, 5);
+        sendKeysSafely(passwordField, password, 5);
+        sendKeysSafely(repeatPasswordField, password, 5);
+    }
+
+    public String getSuccessAlertMessage() {
+        return wait.until(ExpectedConditions.visibilityOf(successMessage)).getText();
+    }
 }
